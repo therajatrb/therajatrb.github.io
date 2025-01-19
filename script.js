0405c48db547682536bc6e1b2866631ebfe9591d
Rajat Kumar <rajat.kumarcd.mec14@itbhu.ac.in>
2025-01-20 00:22:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+const toggleButton = document.createElement('button');
+toggleButton.textContent = 'Toggle Dark Mode';
+document.body.prepend(toggleButton);
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
